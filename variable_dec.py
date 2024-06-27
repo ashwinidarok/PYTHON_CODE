5546bc6a6d84911b3be6d193509efe6222fe83ab
ashwinidarok <ashwinidarokar@gmail.com>
2024-06-27 20:05:21 +0530

    Few more lines

---
+++ variable_dec.py
@@ -0,0 +1,16 @@
+num1=200
+name="cdac"
+status = True
+num2=40
+print(type(num1))
+print(type(name))
+print(type(status))
+sum = num1 + num2
+print(sum)
+
+#ghhjjkkkkk
+"""
+ghhjhjh
+dsgdfh
+hghf
+"""
